b78e9d671f2c5017f415989770033700c016f5b6
Luke Anthony <ln64.ohio@gmail.com>
2025-08-20 20:53:34 -0400

    setup for tempMail

--- src/jackpota/User/User.ts
+++ src/jackpota/User/User.ts
@@ -13,12 +13,21 @@ export class User {
   public async initialize(): Promise<void> {
     const location = await User.fetchLocation();
     const genUser = await User.fetchUser();
+    const tempMail = await User.fetchTempMailAddress()
     if (!genUser) throw new Error("No user data returned from API");
     this.name = `${genUser.name.first} ${genUser.name.last}`;
-    this.email = genUser.email ?? "user@example.com";
+    this.email = tempMail;
     this.dob = genUser.dob.date; // Extract the date string from the dob object
     this.location = location?.region || "NY";
     this.password = "Password123!";
+  }
+
+  static async fetchTempMailAddress(): Promise<string> {
+    return ""
+  }
+
+  static async fetchTempMailCode(): Promise<string> {
+    return ""
   }
 
   static async fetchLocation(): Promise<GeolocationResponse | null> {
